15e8241ba20e3e25f05a9da0a455c4e5dcf069f4
imraxking <161598928+imraxking@users.noreply.github.com>
2024-05-28 16:50:31 +0000

    python error removed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 echo "================="
+# Activate the virtual environment
+source /opt/venv/bin/activate
 
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUR_EMAIL}"
 git config --global --add safe.directory  /github/workspace
 
-python3 /usr/bin/feed.py
+# Execute the feed.py script
+python /usr/bin/feed.py
 
 git add -A && git commit -m "Update Feed"
 git push --set-upstream origin main
